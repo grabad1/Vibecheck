57015763b8a3a1d120d5e52041a9cb64164eaebf
DzoniS <sn220281d@student.etf.bg.ac.rs>
2025-10-14 13:14:43 +0200

    ispravljene instrukcije na help stranici
    Change-Id: I201de15d5ce9ed8da0fc51402057642562f0f9c5

--- django/VibeCheck/VibeCheck/settings.py
+++ django/VibeCheck/VibeCheck/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vibecheck',
         'USER': 'root',
-        'PASSWORD': 'root123',
+        'PASSWORD': 'Stefantubic!123',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
